1a72b3c1ad321ef955a1b5ab5918825c80e3c72f
Tim <faratim@gmail.com>
2024-12-19 08:05:28 -0500

    adding middleware, protecting routes, custom sign in page

--- app/_components/Navigation.js
+++ app/_components/Navigation.js
@@ -1,6 +1,10 @@
 import Link from "next/link";
+import { auth } from "../_lib/auth";
 
-export default function Navigation() {
+export default async function Navigation() {
+  const sessions = await auth();
+  console.log(sessions);
+
   return (
     <nav className="z-10 text-xl">
       <ul className="flex gap-16 items-center">
@@ -21,12 +25,27 @@ export default function Navigation() {
           </Link>
         </li>
         <li>
-          <Link
-            href="/account"
-            className="hover:text-accent-400 transition-colors"
-          >
-            Guest area
-          </Link>
+          {sessions?.user?.image ? (
+            <Link
+              href="/account"
+              className="hover:text-accent-400 transition-colors flex items-center gap-2"
+            >
+              <img
+                className="h-8 rounded-full"
+                src={sessions.user.image}
+                alt={sessions.user.name}
+                referrerPolicy="no-referrer"
+              />
+              <span>Guest area</span>
+            </Link>
+          ) : (
+            <Link
+              href="/account"
+              className="hover:text-accent-400 transition-colors"
+            >
+              Guest area
+            </Link>
+          )}
         </li>
       </ul>
     </nav>
